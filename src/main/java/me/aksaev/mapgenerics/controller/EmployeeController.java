cc85fc391b216df397b5867369387a2135f00f38
Aksaev <mail-ax@ya.ru>
2023-04-05 20:53:41 +0400

    ДЗ 2.7. Коллекции: многообразие реализаций Выполнил

---
+++ src/main/java/me/aksaev/mapgenerics/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package me.aksaev.mapgenerics.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import me.aksaev.mapgenerics.model.Employee;
+import me.aksaev.mapgenerics.service.EmployeeService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/add")
+    public Employee addEmployee(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.addEmployee(firstName, lastName);
+    }
+
+    @GetMapping("/remove")
+    public Employee removeEmployee(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.removeEmployee(firstName, lastName);
+    }
+
+    @GetMapping("/find")
+    public Employee findEmployee(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.findEmployee(firstName, lastName);
+    }
+
+}
